dff2f60ecc1e65e28e109118ddad8771ea90b8bf
Чураков Павел <pschurakov@miem.hse.ru>
2023-08-28 18:20:38 +0300

    + postMessages

--- src/App.js
+++ src/App.js
@@ -48,12 +48,13 @@ function App() {
 
   useEffect(() => {
     if (editor) {
-      window.parent.postMessage({type: 'editor_init', main: 'editor'});
+      window.parent.postMessage(JSON.stringify({type: 'editor_init', main: 'editor'}));
       window.addEventListener('message', (e) => {
           console.log(e.data);
-          if (e.data.type === 'second') {
-            editor.setContent(e.data.main);
-            window.parent.postMessage({type: 'second', main: 'done'}, '*');
+          const data = JSON.parse(e.data);
+          if (data.type === 'second') {
+            editor.setContent(data.main);
+            window.parent.postMessage(JSON.stringify({type: 'second', main: 'done'}), '*');
         }
       })
     }
