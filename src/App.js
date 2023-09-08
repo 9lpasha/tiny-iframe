b88e70b274483d5c05990ebb37d2c7d8cdca1b8c
Чураков Павел <pschurakov@miem.hse.ru>
2023-09-08 22:00:41 +0300

    fix files and language

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
             // изменение высота, на которую влияют файлы
             const node = document.querySelector('.tox-tinymce');
             // eslint-disable-next-line no-magic-numbers
-            const n = data.files ? data.files.length : 0;
+            const n = data.value.files ? data.value.files.length : 0;
 
             if (node) {
               const classText = 'tox-tinymce-files';
