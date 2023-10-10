bc73ec042e226663fe2d67981d02d46b26ec3862
apple <apple@apples-iMac.local>
2023-10-10 17:06:04 +0300

    + PUBLIC_URL

--- scripts/prebuild.js
+++
@@ -1,4 +0,0 @@
-process.env['PUBLIC_URL'] = process.env['TINYMCE_URL'] === 'https://support.happydesk.ru/tinymce' ? '/tinymce' : '/test-tinymce';
-
-console.log(process.env['TINYMCE_URL']);
-console.log(process.env['PUBLIC_URL'])
