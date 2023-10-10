98f4c047fa6315e1b5a7b5c7ffc22b2d13681e2e
apple <apple@apples-iMac.local>
2023-10-10 18:45:52 +0300

    + vite

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM harbor.dev.socialsys.ru/public/nginx:stable-alpine3.17-slim
 
 ADD ./ /www/code
 
-ADD ./nginx.conf /etc/nginx/conf.d/default.conf
+ADD nginx/nginx.conf /etc/nginx/conf.d/default.conf
 
 RUN sed -i "s/http {/http {\n        client_max_body_size 10M;/" /etc/nginx/nginx.conf
---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import inject from 'rollup-plugin-inject';
+import { config } from 'dotenv';
+
+config();
+
+const { TINYMCE_URL } = process.env;
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: { outDir: 'build', },
+  plugins: [
+    react(),
+    inject({
+      include: ['src/**/*.js', 'src/**/*.jsx'],
+    }),
+  ],
+  esbuild: {
+    loader: 'jsx',
+    include: /.*\.jsx?$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+  base: TINYMCE_URL,
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        additionalData: '@import "/src/theme/colors.less";',
+      },
+    },
+  },
+});
