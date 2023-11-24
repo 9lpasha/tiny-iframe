bfb660ca4327f99f2bb646a3f72d8b154e979049
apple <apple@apples-iMac.local>
2023-11-24 14:22:18 +0300

    HD-3819 random hash in builds

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,25 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import inject from "rollup-plugin-inject";
 import { config } from "dotenv";
+import { randomBytes } from "crypto";
 
 config();
 
+function generateRandomString(length) {
+  return randomBytes(Math.ceil(length / 2))
+    .toString("hex")
+    .slice(0, length);
+}
+
 export default defineConfig({
-  build: { outDir: "build" },
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      output: {
+        assetFileNames: `assets/[name]-${generateRandomString(8)}[extname]`,
+      },
+    },
+  },
   plugins: [
     react(),
     inject({
